b1e0ecedd745e3a8ecc7fbdeef48a889063cec1b
osmankbk <mackiekbk@yahoo.com>
2025-12-18 17:11:10 -0500

    Created the Button component and style the sign up, form-input component

--- src/components/sign-up-form/sign-up-form.component.jsx
+++ src/components/sign-up-form/sign-up-form.component.jsx
@@ -1,5 +1,8 @@
 import { useState } from "react";
 import { createAuthUserWithEmailAndPassword, createUserDocumentFromAuth } from "../../utils/firebase/firebase.utils";
+import FormInput from "../form-input/form-input.component";
+import Button from "../button/button.component";
+import './sign-up-form.styles.scss';
 
 const defaultFormFields = {
   displayName: '',
@@ -44,21 +47,18 @@ const SignUpForm  = () => {
   }
 
   return (
-    <div>
-      <h1>Sign up with your email and password</h1>
+    <div className="sign-up-container">
+      <h2>Don't have an account?</h2>
+      <span>Sign up with your email and password</span>
       <form onSubmit={ handleSubmit }>
-        <label>Display Name</label>
-        <input type='text' required onChange={handleChange} name="displayName" value={displayName} />
+        <FormInput label="Display Name" required onChange={handleChange} name="displayName" value={displayName} />
 
-        <label>Email</label>
-        <input type='email' required onChange={handleChange} name="email" value={email}/>
+        <FormInput label="Email" required onChange={handleChange} name="email" value={email} />
 
-        <label>Password</label>
-        <input type='password' required onChange={handleChange} name="password" value={password} />
+        <FormInput label="Password" required onChange={handleChange} name="password" value={password} />
 
-        <label>Confirm Password</label>
-        <input type='password' required onChange={handleChange} name="confirmPassword" value={confirmPassword}/>
-        <button type="submit">Submit</button>
+        <FormInput label="Confirm Password" required onChange={handleChange} name="confirmPassword" value={confirmPassword} />
+        <Button type="Submit">Sign Up</Button>
       </form>
     </div>
   );
